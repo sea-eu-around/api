cff670ea1be9c7d8a9edf7ff4119017b4fc679fa
Ladislas Dellinger <ladislas14@gmail.com>
2020-08-11 12:02:03 +0200

    other: add travis.ci file

--- src/entities/teacherProfile.entity.ts
+++ src/entities/teacherProfile.entity.ts
@@ -1,12 +1,31 @@
-import { Column, Entity } from 'typeorm';
+import { Column, Entity, JoinColumn, OneToOne } from 'typeorm';
 
+import { AbstractEntity } from '../common/abstract.entity';
 import { TeacherProfileDto } from '../dto/TeacherProfileDto';
-import { ProfileEntity } from './abstract/profile.entity';
+import { IProfile } from './interfaces/profile.interface';
+import { UserEntity } from './user.entity';
 
 @Entity({ name: 'teacher_profile' })
-export class TeacherProfileEntity extends ProfileEntity {
+export class TeacherProfileEntity extends AbstractEntity<TeacherProfileDto>
+    implements IProfile {
     @Column()
-    salary: number;
+    firstName: string;
+
+    @Column()
+    lastName: string;
+
+    @Column()
+    username: string;
+
+    @Column()
+    city: string;
+
+    @Column()
+    university: string;
+
+    @OneToOne(() => UserEntity, (user) => user.teacherProfile)
+    @JoinColumn()
+    user: UserEntity;
 
     dtoClass = TeacherProfileDto;
 }
--- src/modules/auth/auth.controller.ts
+++ src/modules/auth/auth.controller.ts
@@ -5,25 +5,16 @@ import {
     HttpCode,
     HttpStatus,
     Post,
-    UploadedFile,
     UseGuards,
     UseInterceptors,
 } from '@nestjs/common';
-import { FileInterceptor } from '@nestjs/platform-express';
-import {
-    ApiBearerAuth,
-    ApiConsumes,
-    ApiOkResponse,
-    ApiTags,
-} from '@nestjs/swagger';
+import { ApiBearerAuth, ApiOkResponse, ApiTags } from '@nestjs/swagger';
 
 import { AuthUser } from '../../decorators/auth-user.decorator';
-import { ApiFile } from '../../decorators/swagger.schema';
 import { UserDto } from '../../dto/UserDto';
 import { UserEntity } from '../../entities/user.entity';
 import { AuthGuard } from '../../guards/auth.guard';
 import { AuthUserInterceptor } from '../../interceptors/auth-user-interceptor.service';
-import { IFile } from '../../interfaces/IFile';
 import { UserService } from '../user/user.service';
 import { AuthService } from './auth.service';
 import { LoginPayloadDto } from './dto/LoginPayloadDto';
@@ -56,17 +47,10 @@ export class AuthController {
     @Post('register')
     @HttpCode(HttpStatus.OK)
     @ApiOkResponse({ type: UserDto, description: 'Successfully Registered' })
-    @ApiConsumes('multipart/form-data')
-    @ApiFile('avatar')
-    @UseInterceptors(FileInterceptor('avatar'))
     async userRegister(
         @Body() userRegisterDto: UserRegisterDto,
-        @UploadedFile() file: IFile,
     ): Promise<UserDto> {
-        const createdUser = await this.userService.createUser(
-            userRegisterDto,
-            file,
-        );
+        const createdUser = await this.userService.createUser(userRegisterDto);
 
         return createdUser.toDto();
     }
@@ -77,7 +61,7 @@ export class AuthController {
     @UseInterceptors(AuthUserInterceptor)
     @ApiBearerAuth()
     @ApiOkResponse({ type: UserDto, description: 'current user info' })
-    getCurrentUser(@AuthUser() user: UserEntity) {
+    getCurrentUser(@AuthUser() user: UserEntity): Promise<UserDto> {
         return user.toDto();
     }
 }
