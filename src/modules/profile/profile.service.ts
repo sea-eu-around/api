6a415884f69792975d4c59ffcbd8045b7b626b65
Ladislas Dellinger <ladislas14@gmail.com>
2020-10-05 15:30:25 +0200

    chore: change teacher to staff

--- src/modules/profile/profile.service.ts
+++ src/modules/profile/profile.service.ts
@@ -2,29 +2,29 @@
 import { Injectable } from '@nestjs/common';
 
 import { ProfileType } from '../../common/constants/profile-type';
+import { StaffProfileEntity } from '../../entities/staffProfile.entity';
 import { StudentProfileEntity } from '../../entities/studentProfile.entity';
-import { TeacherProfileEntity } from '../../entities/teacherProfile.entity';
 import { UserEntity } from '../../entities/user.entity';
+import { StaffProfileRepository } from '../../repositories/staffProfile.repository';
 import { StudentProfileRepository } from '../../repositories/studentProfile.repository';
-import { TeacherProfileRepository } from '../../repositories/teacherProfile.repository';
 import { ProfileCreationDto } from './dto/ProfileCreationDto';
 
 @Injectable()
 export class ProfileService {
     constructor(
         private readonly _studentProfileRepository: StudentProfileRepository,
-        private readonly _teacherProfileRepository: TeacherProfileRepository,
+        private readonly _staffProfileRepository: StaffProfileRepository,
     ) {}
 
     async createProfile(
         profileCreationDto: ProfileCreationDto,
         type: ProfileType,
         user: UserEntity,
-    ): Promise<StudentProfileEntity | TeacherProfileEntity> {
+    ): Promise<StudentProfileEntity | StaffProfileEntity> {
         const profileRepository =
             type === ProfileType.STUDENT
                 ? this._studentProfileRepository
-                : this._teacherProfileRepository;
+                : this._staffProfileRepository;
 
         const profile = profileRepository.create();
         Object.assign(profile, profileCreationDto);
