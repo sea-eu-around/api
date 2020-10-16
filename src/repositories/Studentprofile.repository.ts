fb248d93c3ff264d5a36cd626db4c87c9828e4e8
Ladislas Dellinger <ladislas14@gmail.com>
2020-10-16 17:43:07 +0200

    try to fix problem on student repository

--- src/repositories/Studentprofile.repository.ts
+++
@@ -1,9 +0,0 @@
-import { Repository } from 'typeorm';
-import { EntityRepository } from 'typeorm/decorator/EntityRepository';
-
-import { StudentProfileEntity } from '../entities/studentProfile.entity';
-
-@EntityRepository(StudentProfileEntity)
-export class StudentProfileRepository extends Repository<
-    StudentProfileEntity
-> {}
