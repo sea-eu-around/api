eb0a8a613064105d37659a1134f5408014d9a7ea
Ladislas Dellinger <ladislas14@gmail.com>
2020-10-26 16:25:09 +0100

    add offer entity

---
+++ src/dto/OfferDto.ts
@@ -0,0 +1,3 @@
+import { AbstractDto } from '../common/dto/AbstractDto';
+
+export class OfferDto extends AbstractDto {}
---
+++ src/entities/offer.entity.ts
@@ -0,0 +1,22 @@
+import { Column, Entity } from 'typeorm';
+
+import { AbstractEntity } from '../common/abstract.entity';
+import { OffersCategoryType } from '../common/constants/offers-category-type';
+import { OfferDto } from '../dto/OfferDto';
+
+@Entity('offer')
+export class OfferEntity extends AbstractEntity<OfferDto> {
+    @Column()
+    key: string;
+
+    @Column()
+    target: number;
+
+    @Column({ type: 'enum', enum: OffersCategoryType })
+    category: OffersCategoryType;
+
+    @Column()
+    allowInterRole: boolean;
+
+    dtoClass = OfferDto;
+}
