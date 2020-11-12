78829ef9c2bab03ed64661177d14d4443a32a8c0
Alfred <alfred.pichard@imt-atlantique.net>
2020-11-12 16:18:44 +0100

    change from profile to user + response data

--- src/entities/matching.entity.ts
+++ src/entities/matching.entity.ts
@@ -3,15 +3,15 @@ import { Column, Entity, ManyToOne } from 'typeorm';
 import { AbstractEntity } from '../common/abstract.entity';
 import { MatchingStatusType } from '../common/constants/matching-status-type';
 import { MatchingDto } from '../dto/MatchingDto';
-import { ProfileEntity } from './profile.entity';
+import { UserEntity } from './user.entity';
 
 @Entity('matching')
 export class MatchingEntity extends AbstractEntity<MatchingDto> {
-    @ManyToOne(() => ProfileEntity, (profile) => profile.givenLikes)
-    fromProfile: ProfileEntity;
+    @ManyToOne(() => UserEntity, (user) => user.givenLikes)
+    fromUser: UserEntity;
 
-    @ManyToOne(() => ProfileEntity, (profile) => profile.receivedLikes)
-    toProfile: ProfileEntity;
+    @ManyToOne(() => UserEntity, (user) => user.receivedLikes)
+    toUser: UserEntity;
 
     @Column({
         type: 'enum',
