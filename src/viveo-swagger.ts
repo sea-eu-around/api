721f97aa71f7f63c65a51347a30969b6c9548144
Ladislas Dellinger <ladislas14@gmail.com>
2020-11-21 21:44:04 +0100

    test for auto update

--- src/viveo-swagger.ts
+++ src/viveo-swagger.ts
@@ -3,7 +3,7 @@ import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
 
 export function setupSwagger(app: INestApplication): void {
     const options = new DocumentBuilder()
-        .setTitle('API')
+        .setTitle('SEA-EU Around API')
         .setVersion('0.0.1')
         .addBearerAuth()
         .build();
