a3194a91b7b3ee2f97a57196f1155a56b7cc0f31
Ladislas Dellinger <ladislas14@gmail.com>
2020-11-29 15:06:52 +0100

    use profiles instead of users for chat

---
+++ src/entities/profileRoom.entity.ts
@@ -0,0 +1,30 @@
+import { Column, Entity, Index, ManyToOne, PrimaryColumn } from 'typeorm';
+
+import { AbstractCompositeEntity } from '../common/abstractComposite.entity';
+import { ProfileRoomDto } from '../dto/ProfileRoomDto';
+import { ProfileEntity } from './profile.entity';
+import { RoomEntity } from './room.entity';
+
+@Entity('profile_room')
+export class ProfileRoomEntity extends AbstractCompositeEntity<ProfileRoomDto> {
+    @Index()
+    @Column()
+    @PrimaryColumn()
+    profileId!: string;
+
+    @Index()
+    @Column()
+    @PrimaryColumn()
+    roomId!: string;
+
+    @ManyToOne(() => ProfileEntity, (profile) => profile.profileRooms)
+    profile: ProfileEntity;
+
+    @ManyToOne(() => RoomEntity, (room) => room.profileRooms)
+    room: RoomEntity;
+
+    @Column({ nullable: true })
+    lastMessageSeenId?: string;
+
+    dtoClass = ProfileRoomDto;
+}
--- src/modules/room/room.module.ts
+++ src/modules/room/room.module.ts
@@ -1,13 +1,15 @@
 import { Module } from '@nestjs/common';
 import { TypeOrmModule } from '@nestjs/typeorm';
 
+import { ProfileRoomRepository } from '../../repositories/profileRoom.repository';
 import { RoomRepository } from '../../repositories/room.repository';
-import { UserRoomRepository } from '../../repositories/userRoom.repository';
 import { RoomController } from './room.controller';
 import { RoomService } from './room.service';
 
 @Module({
-    imports: [TypeOrmModule.forFeature([RoomRepository, UserRoomRepository])],
+    imports: [
+        TypeOrmModule.forFeature([RoomRepository, ProfileRoomRepository]),
+    ],
     controllers: [RoomController],
     providers: [RoomService],
 })
