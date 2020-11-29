4e626d97d29813900f4e73bb2ea9c917fd4af36e
Alfred <alfred.pichard@imt-atlantique.net>
2020-11-29 14:21:40 +0100

    change from user to profile

--- src/modules/matching/dto/toProfileDto.ts
+++ src/modules/matching/dto/toProfileDto.ts
@@ -1,8 +1,8 @@
 import { ApiProperty } from '@nestjs/swagger';
 import { IsString } from 'class-validator';
 
-export class ToUserDto {
+export class ToProfileDto {
     @ApiProperty()
     @IsString()
-    readonly toUserId: string;
+    readonly toProfileId: string;
 }
