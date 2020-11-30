10068a217709aa53223d9ca698c8f06584f82ad3
Ladislas Dellinger <ladislas14@gmail.com>
2020-11-30 19:49:43 +0100

    add sendMessage
    with missing notifications and message to people in chat page

--- src/modules/message/message.gateway.ts
+++ src/modules/message/message.gateway.ts
@@ -14,11 +14,13 @@ import {
     WsException,
     WsResponse,
 } from '@nestjs/websockets';
+import { profile } from 'console';
 import { Socket } from 'socket.io';
 import { Server } from 'ws';
 import { WsAuthUser } from '../../decorators/ws-auth-user.decorator';
 import { UserEntity } from '../../entities/user.entity';
 import { WsJwtAuthGuard } from '../../guards/wsJwtAuth.guard';
+import { MessageRepository } from '../../repositories/message.repository';
 import { ProfileRoomRepository } from '../../repositories/profileRoom.repository';
 import { RoomRepository } from '../../repositories/room.repository';
 import { UserService } from '../user/user.service';
@@ -32,32 +34,47 @@ export class MessageGateway
     @WebSocketServer() server: Server;
 
     private _logger: Logger;
-    private _onlineUsers: Set<any>;
+    private _onlineProfiles: Set<any>;
 
-    constructor(private readonly _profileRoomRepository: ProfileRoomRepository, private readonly _jwtService: JwtService, private readonly _userService: UserService, private readonly _roomRepository: RoomRepository){
+    constructor(private readonly _profileRoomRepository: ProfileRoomRepository, private readonly _jwtService: JwtService, private readonly _userService: UserService, private readonly _roomRepository: RoomRepository, private readonly _messageRepository: MessageRepository){
         this._logger = new Logger('MessageGateway');
-        this._onlineUsers = new Set();
+        this._onlineProfiles = new Set();
 
     }
 
     @SubscribeMessage('sendMessage')
     async sendMessage(@WsAuthUser() user: UserEntity, @MessageBody() data: SendMessageDto, @ConnectedSocket() client: Socket): Promise<WsResponse<any>> {
         const room = await this._roomRepository.findOne({where: {id: data.roomId}, relations: ['profiles']})
-        return this.server.to(data.roomId).emit('receiveMessage', data);
+        const offlineProfileIds = room.profiles.map(x => x.profileId).filter(profileId => profileId in this._onlineProfiles);
+
+        // TODO: send notification to offline profile
+
+        let message = this._messageRepository.create()
+        message.text = data.text;
+        message.roomId = data.roomId;
+        message.senderId = user.id;
+        message.sent = true;
+        message = await this._messageRepository.save(message);
+
+        room.lastMessage = message;
+        await this._roomRepository.save(room);
+
+        return this.server.to(data.roomId).emit('receiveMessage', message.toDto());
         
     }
 
     @SubscribeMessage('joinRoom')
     async joinRoom(@WsAuthUser() user: UserEntity, @MessageBody() data: JoinRoomDto, @ConnectedSocket() client: Socket): Promise<void> {
         const isProfileInRoom = await this._profileRoomRepository.isProfileInRoom(user.id, data.roomId)
+        console.log(isProfileInRoom);
         if (data.roomId !== user.id && !isProfileInRoom) {
             throw new WsException('Forbidden');
         }
 
         client.join(data.roomId);
 
-        this._onlineUsers[user.id] = data.roomId;
-        this._logger.log(this._onlineUsers);
+        this._onlineProfiles[user.id] = data.roomId;
+        this._logger.log(this._onlineProfiles);
 
         client.emit('joinedRoom', data.roomId);
     }
@@ -66,8 +83,8 @@ export class MessageGateway
     public leaveRoom(@WsAuthUser() user: UserEntity, @MessageBody() data: JoinRoomDto, @ConnectedSocket() client: Socket): void {
         client.leave(data.roomId);
 
-        this._onlineUsers[user.id] = data.roomId;
-        this._logger.log(this._onlineUsers);
+        this._onlineProfiles[user.id] = data.roomId;
+        this._logger.log(this._onlineProfiles);
 
         client.emit('leftRoom', data.roomId);
     }
@@ -93,8 +110,8 @@ export class MessageGateway
             throw new WsException('Unauthorized');
         }
 
-        this._onlineUsers[user.id] = client.id;
-        this._logger.log(this._onlineUsers);
+        this._onlineProfiles[user.id] = client.id;
+        this._logger.log(this._onlineProfiles);
 
         return this._logger.log(`Client connected: ${client.id}`);
     }
