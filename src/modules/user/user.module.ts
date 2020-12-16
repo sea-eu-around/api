95010238124c2654f998f355cd9e41f584f5da84
Ladislas Dellinger <ladislas14@gmail.com>
2020-12-16 17:44:32 +0100

    add deletedAt and isActive and update it on user deletion

--- src/modules/user/user.module.ts
+++ src/modules/user/user.module.ts
@@ -1,12 +1,13 @@
 import { Module } from '@nestjs/common';
 import { TypeOrmModule } from '@nestjs/typeorm';
 
+import { ProfileRepository } from '../../repositories/profile.repository';
 import { UserController } from './user.controller';
 import { UserRepository } from './user.repository';
 import { UserService } from './user.service';
 
 @Module({
-    imports: [TypeOrmModule.forFeature([UserRepository])],
+    imports: [TypeOrmModule.forFeature([UserRepository, ProfileRepository])],
     controllers: [UserController],
     exports: [UserService],
     providers: [UserService],
