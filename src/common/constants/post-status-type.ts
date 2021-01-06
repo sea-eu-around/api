c89f7471d93c49831e19587ee53918f560d093cc
Alfred <alfred.pichard@imt-atlantique.net>
2021-01-06 16:38:56 +0100

    Revert "entities & dtos"
    This reverts commit e7acae27ccf775d55365135ce84217dd1dda2dbb.

--- src/common/constants/post-status-type.ts
+++
@@ -1,7 +0,0 @@
-'use-strict';
-
-export enum PostStatusType {
-    APPROVED = 'approved',
-    PENDING = 'pending',
-    REFUSED = 'refused',
-}
--- src/common/constants/post-type.ts
+++
@@ -1,6 +0,0 @@
-'use-strict';
-
-export enum PostType {
-    SIMPLE = 'simple',
-    MEDIA = 'media',
-}
