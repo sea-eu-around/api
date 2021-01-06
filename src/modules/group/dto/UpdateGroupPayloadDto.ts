7495b9ade2bab28b5a351df3c82448d7c37e6406
Ladislas Dellinger <ladislas14@gmail.com>
2021-01-06 12:20:23 +0100

    create and update

---
+++ src/modules/group/dto/UpdateGroupPayloadDto.ts
@@ -0,0 +1,19 @@
+import { ApiProperty, ApiPropertyOptional } from '@nestjs/swagger';
+import { IsBoolean, IsOptional, IsString } from 'class-validator';
+
+export class UpdateGroupPayloadDto {
+    @ApiPropertyOptional()
+    @IsString()
+    @IsOptional()
+    readonly name!: string;
+
+    @ApiProperty()
+    @IsBoolean()
+    @IsOptional()
+    readonly visible!: boolean;
+
+    @ApiPropertyOptional()
+    @IsBoolean()
+    @IsOptional()
+    readonly requireApproval!: boolean;
+}
