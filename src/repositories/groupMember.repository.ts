e43ec45667d45f1ade1be795a633dc6535e96b9a
Ladislas Dellinger <ladislas14@gmail.com>
2021-01-06 14:13:18 +0100

    fix isAdmin

--- src/repositories/groupMember.repository.ts
+++ src/repositories/groupMember.repository.ts
@@ -5,13 +5,17 @@ import { GroupMemberEntity } from '../entities/groupMember.entity';
 
 @EntityRepository(GroupMemberEntity)
 export class GroupMemberRepository extends Repository<GroupMemberEntity> {
-    async isMemberAdmin(profileId: string, groupId: string): Promise<boolean> {
-        return (
-            (await this.findOne({
-                profileId,
-                groupId,
-                role: GroupMemberRoleType.ADMIN,
-            })) !== null
-        );
+    async isAdmin(profileId: string, groupId: string): Promise<boolean> {
+        const groupMember = await this.findOne({
+            profileId,
+            groupId,
+            role: GroupMemberRoleType.ADMIN,
+        });
+
+        if (groupMember) {
+            return true;
+        }
+
+        return false;
     }
 }
