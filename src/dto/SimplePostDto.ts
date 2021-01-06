847e2570f5e363bc495d0ff7dbd578d5c00cc4cc
Alfred <alfred.pichard@imt-atlantique.net>
2021-01-06 16:43:12 +0100

    Revert "simplePost entity"
    This reverts commit c6541b79c82cc505f6472037ea2dd45d16389e19.

--- src/dto/SimplePostDto.ts
+++
@@ -1,8 +0,0 @@
-import { SimplePostEntity } from '../entities/simplePost.entity';
-import { PostDto } from './PostDto';
-
-export class SimplePostDto extends PostDto {
-    constructor(post: SimplePostEntity) {
-        super(post);
-    }
-}
