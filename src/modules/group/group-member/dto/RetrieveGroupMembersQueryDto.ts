3f5671b6a1ca45a6b3506edf8234d4e2fc7dc6d6
Ladislas Dellinger <ladislas14@gmail.com>
2021-01-08 16:08:14 +0100

    Clean group module organisation

--- src/modules/group/group-member/dto/RetrieveGroupMembersQueryDto.ts
+++ src/modules/group/group-member/dto/RetrieveGroupMembersQueryDto.ts
@@ -1,7 +1,7 @@
 import { Transform } from 'class-transformer';
 import { IsArray, IsEnum, IsInt, IsOptional } from 'class-validator';
 
-import { GroupMemberStatusType } from '../../../common/constants/group-member-status-type';
+import { GroupMemberStatusType } from '../../../../common/constants/group-member-status-type';
 
 export class GetManyGroupMembersQueryDto {
     @IsInt()
--- src/modules/group/group.module.ts
+++ src/modules/group/group.module.ts
@@ -3,12 +3,14 @@ import { TypeOrmModule } from '@nestjs/typeorm';
 
 import { GroupRepository } from '../../repositories/group.repository';
 import { GroupMemberRepository } from '../../repositories/groupMember.repository';
+import { GroupMemberModule } from './group-member/group-member.module';
 import { GroupController } from './group.controller';
 import { GroupService } from './group.service';
 
 @Module({
     imports: [
         TypeOrmModule.forFeature([GroupRepository, GroupMemberRepository]),
+        GroupMemberModule,
     ],
     providers: [GroupService],
     controllers: [GroupController],
