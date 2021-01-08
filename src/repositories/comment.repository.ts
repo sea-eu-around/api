32d8b3ce144a02076dafe79461f192dd9e9e0377
Ladislas Dellinger <ladislas14@gmail.com>
2021-01-08 16:17:33 +0100

    Remove comments

--- src/repositories/comment.repository.ts
+++
@@ -1,5 +0,0 @@
-import { EntityRepository, TreeRepository } from 'typeorm';
-
-import { CommentEntity } from '../entities/comment.entity';
-@EntityRepository(CommentEntity)
-export class CommentRepository extends TreeRepository<CommentEntity> {}
