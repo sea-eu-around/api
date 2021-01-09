0feae27cf9bb63bbd4c10067ac965144c1172646
Ladislas Dellinger <ladislas14@gmail.com>
2021-01-09 14:23:53 +0100

    implement module + retrieve

--- src/modules/group/group.module.ts
+++ src/modules/group/group.module.ts
@@ -1,8 +1,9 @@
 import { Module } from '@nestjs/common';
 import { TypeOrmModule } from '@nestjs/typeorm';
 
+import { GroupMemberRepository } from '../../repositories/group-member.repository';
 import { GroupRepository } from '../../repositories/group.repository';
-import { GroupMemberRepository } from '../../repositories/groupMember.repository';
+import { CommentModule } from './comment/comment.module';
 import { GroupController } from './group.controller';
 import { GroupService } from './group.service';
 import { PostModule } from './post/post.module';
@@ -11,6 +12,7 @@ import { PostModule } from './post/post.module';
     imports: [
         TypeOrmModule.forFeature([GroupRepository, GroupMemberRepository]),
         PostModule,
+        CommentModule,
     ],
     providers: [GroupService],
     controllers: [GroupController],
