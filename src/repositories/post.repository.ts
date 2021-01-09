cf70f255118d6cc772c31aec84cc03753c39314a
Ladislas Dellinger <ladislas14@gmail.com>
2021-01-09 19:21:42 +0100

    remove useless isEmpty

--- src/repositories/post.repository.ts
+++ src/repositories/post.repository.ts
@@ -4,12 +4,4 @@ import { EntityRepository } from 'typeorm/decorator/EntityRepository';
 import { PostEntity } from '../entities/post.entity';
 
 @EntityRepository(PostEntity)
-export class PostRepository extends Repository<PostEntity> {
-    async isEmpty({ groupId }: { groupId: string }): Promise<boolean> {
-        const post = await this.findOne({ groupId });
-        if (post) {
-            return false;
-        }
-        return true;
-    }
-}
+export class PostRepository extends Repository<PostEntity> {}
