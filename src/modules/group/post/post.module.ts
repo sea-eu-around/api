d274d951641703c6391d5a212c0d71436c62dbca
Ladislas Dellinger <ladislas14@gmail.com>
2021-01-09 14:38:14 +0100

    reorganisation

---
+++ src/modules/group/post/post.module.ts
@@ -0,0 +1,21 @@
+import { Module } from '@nestjs/common';
+import { TypeOrmModule } from '@nestjs/typeorm';
+
+import { GroupRepository } from '../../../repositories/group.repository';
+import { GroupMemberRepository } from '../../../repositories/groupMember.repository';
+import { PostRepository } from '../../../repositories/post.repository';
+import { PostController } from './post.controller';
+import { PostService } from './post.service';
+
+@Module({
+    imports: [
+        TypeOrmModule.forFeature([
+            GroupRepository,
+            PostRepository,
+            GroupMemberRepository,
+        ]),
+    ],
+    providers: [PostService],
+    controllers: [PostController],
+})
+export class PostModule {}
