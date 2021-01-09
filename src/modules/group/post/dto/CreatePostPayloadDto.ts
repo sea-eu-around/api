4f69419d38fc607909ac9f2954afa56e48ffe2f0
Ladislas Dellinger <ladislas14@gmail.com>
2021-01-09 14:47:09 +0100

    Move and rename dto

--- src/modules/group/post/dto/CreatePostPayloadDto.ts
+++ src/modules/group/post/dto/CreatePostPayloadDto.ts
@@ -1,9 +1,9 @@
 import { ApiProperty } from '@nestjs/swagger';
 import { IsString } from 'class-validator';
 
-import { PostType } from '../../../common/constants/post-type';
+import { PostType } from '../../../../common/constants/post-type';
 
-export class CreateGroupPostPayloadDto {
+export class CreatePostPayloadDto {
     @ApiProperty()
     readonly type: PostType;
 
