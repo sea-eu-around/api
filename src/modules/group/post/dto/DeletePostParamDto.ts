1573cc96365dd2f398d0569eabf9e95bb5d8d3a7
Alfred <alfred.pichard@imt-atlantique.net>
2021-01-10 12:58:22 +0100

    cleaning

--- src/modules/group/post/dto/DeletePostParamDto.ts
+++ src/modules/group/post/dto/DeletePostParamDto.ts
@@ -1,9 +1,15 @@
 import { IsString, IsUUID } from 'class-validator';
 
+import { Exists } from '../../../../decorators/exists-validator.decorator';
+import { GroupEntity } from '../../../../entities/group.entity';
+import { PostEntity } from '../../../../entities/post.entity';
+
 export class DeletePostParamDto {
     @IsUUID()
+    @Exists<GroupEntity>(GroupEntity, 'id')
     readonly groupId!: string;
 
     @IsString()
+    @Exists<PostEntity>(PostEntity, 'id')
     readonly id: string;
 }
