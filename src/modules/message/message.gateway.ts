870eae3ef4586fd30d46db545bbfc23ef830c4e0
Ladislas Dellinger <ladislas14@gmail.com>
2021-02-02 12:18:42 +0100

    fix data in message notification

--- src/modules/message/message.gateway.ts
+++ src/modules/message/message.gateway.ts
@@ -116,7 +116,7 @@ export class MessageGateway
 
         for (const offlineOrInOtherRoomUser of offlineOrInOtherRoomUsers) {
             notifications.push({
-                data,
+                data: message.toDto(),
                 to:
                     offlineOrInOtherRoomUser.expoPushToken ||
                     offlineOrInOtherRoomUser.email,
