d4a96682af7bc9a14b9ec4cfeee9967d1c173284
Ladislas Dellinger <ladislas14@gmail.com>
2021-02-17 12:08:23 +0100

    fixes

--- src/modules/group/group.module.ts
+++ src/modules/group/group.module.ts
@@ -5,6 +5,7 @@ import { CommentRepository } from '../../repositories/comment.repository';
 import { GroupCoverRepository } from '../../repositories/group-cover.repository';
 import { GroupMemberRepository } from '../../repositories/group-member.repository';
 import { GroupRepository } from '../../repositories/group.repository';
+import { GroupMemberModule } from './group-member/group-member.module';
 import { GroupController } from './group.controller';
 import { GroupService } from './group.service';
 import { PostModule } from './post/post.module';
@@ -17,6 +18,7 @@ import { PostModule } from './post/post.module';
             GroupCoverRepository,
             CommentRepository,
         ]),
+        GroupMemberModule,
         PostModule,
     ],
     providers: [GroupService],
