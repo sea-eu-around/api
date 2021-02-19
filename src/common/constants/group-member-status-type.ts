6adfc462b31d97beacde187ab1361d5f4dbd943d
Ladislas Dellinger <ladislas14@gmail.com>
2021-02-19 12:15:37 +0100

    add invitations system

--- src/common/constants/group-member-status-type.ts
+++ src/common/constants/group-member-status-type.ts
@@ -4,4 +4,11 @@ export enum GroupMemberStatusType {
     PENDING = 'pending',
     APPROVED = 'approved',
     BANNED = 'banned',
+    INVITED = 'invited',
+    INVITED_BY_ADMIN = 'invited-by-admin',
+}
+
+export enum GroupMemberInvitationStatusType {
+    INVITED = GroupMemberStatusType.INVITED,
+    INVITED_BY_ADMIN = GroupMemberStatusType.INVITED_BY_ADMIN,
 }
--- src/modules/group/group-member/dto/CreateGroupMemberParamsDto.ts
+++ src/modules/group/group-member/dto/CreateGroupMemberParamsDto.ts
@@ -1,6 +1,11 @@
-import { IsUUID } from 'class-validator';
+import { IsEnum, IsUUID } from 'class-validator';
+
+import { GroupMemberInvitationStatusType } from '../../../../common/constants/group-member-status-type';
 
 export class CreateGroupMemberParamsDto {
     @IsUUID()
     readonly groupId!: string;
+
+    @IsEnum(GroupMemberInvitationStatusType)
+    readonly status: GroupMemberInvitationStatusType;
 }
