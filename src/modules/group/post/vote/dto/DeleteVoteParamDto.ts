a8479654137efb2e3e84ffb8ad5269998410e556
Ladislas Dellinger <ladislas14@gmail.com>
2021-02-21 15:34:42 +0100

    fix

--- src/modules/group/post/vote/dto/DeleteVoteParamDto.ts
+++ src/modules/group/post/vote/dto/DeleteVoteParamDto.ts
@@ -3,7 +3,6 @@ import { IsEnum, IsUUID } from 'class-validator';
 import { VoteEntityType } from '../../../../../common/constants/voteEntityType';
 import { Exists } from '../../../../../decorators/exists-validator.decorator';
 import { GroupEntity } from '../../../../../entities/group.entity';
-import { VoteEntity } from '../../../../../entities/vote.entity';
 
 export class DeleteVoteParamDto {
     @IsUUID()
@@ -14,6 +13,5 @@ export class DeleteVoteParamDto {
     readonly entityType!: VoteEntityType;
 
     @IsUUID()
-    @Exists<VoteEntity>(VoteEntity, 'id')
     readonly entityId!: string;
 }
