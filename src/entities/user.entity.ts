1f0c801b43159776fcda723d9b06dbfdc1980c7a
Ladislas Dellinger <ladislas14@gmail.com>
2021-05-27 17:32:22 +0200

    Add last connection feature

--- src/entities/user.entity.ts
+++ src/entities/user.entity.ts
@@ -37,6 +37,9 @@ export class UserEntity extends AbstractEntity<UserDto> {
     @Column({ nullable: true })
     expoPushToken: string;
 
+    @Column({ nullable: true })
+    lastConnection: Date;
+
     // TODO: remove
     verificationToken?: string;
 
